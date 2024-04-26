50c795b29c554c51cda0b0396c8e06eac5117ea6
theodevoid <voidfnc9@gmail.com>
2024-04-26 12:25:51 +0700

    Initial Commit

---
+++ NC1/NC1App.swift
@@ -0,0 +1,17 @@
+//
+//  NC1App.swift
+//  NC1
+//
+//  Created by Theodore Mangowal on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
